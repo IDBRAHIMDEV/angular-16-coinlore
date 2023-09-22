7956d207b2a9505983f6e8cc1e6873e08e784ee4
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2023-09-22 12:52:02 +0100

    generate coins component and a service

--- src/app/coins/coins.module.ts
+++ src/app/coins/coins.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CoinsComponent } from './components/coins/coins.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CoinsComponent
+  ],
   imports: [
     CommonModule
   ]
